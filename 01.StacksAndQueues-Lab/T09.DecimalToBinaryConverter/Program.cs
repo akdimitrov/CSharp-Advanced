65620284d5ef24392a17c72e73246f6aa9774664
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-03-03 23:59:06 +0200

    Added stack and queues lab exercise

---
+++ 01.StacksAndQueues-Lab/T09.DecimalToBinaryConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T09.DecimalToBinaryConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            while (number > 0)
+            {
+                stack.Push(number % 2);
+                number /= 2;
+            }
+
+            Console.WriteLine(stack.Count == 0 ? "0" : string.Join("", stack));
+        }
+    }
+}
