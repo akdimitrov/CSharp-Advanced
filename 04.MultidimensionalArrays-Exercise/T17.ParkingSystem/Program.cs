2fb110c0f75a3b54bd8fdb91044098895dc7cdc3
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-03-09 03:35:57 +0200

    Added More Exercises

---
+++ 04.MultidimensionalArrays-Exercise/T17.ParkingSystem/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace T17.ParkingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[,] parking = new int[size[0], size[1]];
+            string input = Console.ReadLine();
+            while (input != "stop")
+            {
+                int[] cmd = input.Split().Select(int.Parse).ToArray();
+                int entryRow = cmd[0];
+                int row = cmd[1];
+                int col = cmd[2];
+                if (IsRowFree(parking, row))
+                {
+                    FindNearesParkingSpot(parking, row, ref col);
+                    parking[row, col] = 1;
+                    int distance = Math.Abs(row - entryRow) + col + 1;
+                    Console.WriteLine(distance);
+                }
+                else
+                {
+                    Console.WriteLine($"Row {row} full");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static bool IsRowFree(int[,] parking, int row)
+        {
+            for (int col = 1; col < parking.GetLength(1); col++)
+            {
+                if (parking[row, col] == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void FindNearesParkingSpot(int[,] parking, int row, ref int column)
+        {
+            int left = -1;
+            int right = -1;
+            for (int col = column; col >= 1; col--)
+            {
+                if (parking[row, col] == 0)
+                {
+                    left = col;
+                    break;
+                }
+            }
+
+            for (int col = column; col < parking.GetLength(1); col++)
+            {
+                if (parking[row, col] == 0)
+                {
+                    right = col;
+                    break;
+                }
+            }
+
+            bool isRight = (right != -1 && left != -1 && column - left > right - column) || left == -1;
+            column = isRight ? right : left;
+        }
+    }
+}
---
+++ 04.MultidimensionalArrays-Exercise/T18.StringMatrixRotation/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace T18.StringMatrixRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Match number = Regex.Match(Console.ReadLine(), @"\d+");
+            int rotationDegress = int.Parse(number.ToString()) % 360;
+            string input = Console.ReadLine();
+            List<List<char>> jagged = new List<List<char>>();
+            int maxRowLength = 0;
+            while (input != "END")
+            {
+                jagged.Add(new List<char>());
+                jagged[^1].AddRange(input);
+                if (maxRowLength < jagged[^1].Count)
+                {
+                    maxRowLength = jagged[^1].Count;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var row in jagged)
+            {
+                row.AddRange(new string(' ', maxRowLength - row.Count));
+            }
+
+            if (rotationDegress == 90)
+            {
+                for (int col = 0; col < maxRowLength; col++)
+                {
+                    for (int row = jagged.Count - 1; row >= 0; row--)
+                    {
+                        Console.Write(jagged[row][col]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else if (rotationDegress == 180)
+            {
+                for (int row = jagged.Count - 1; row >= 0; row--)
+                {
+                    for (int col = maxRowLength - 1; col >= 0; col--)
+                    {
+                        Console.Write(jagged[row][col]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else if (rotationDegress == 270)
+            {
+                for (int col = maxRowLength - 1; col >= 0; col--)
+                {
+                    for (int row = 0; row < jagged.Count; row++)
+                    {
+                        Console.Write(jagged[row][col]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                foreach (var row in jagged)
+                {
+                    Console.WriteLine(string.Join("", row));
+                }
+            }
+        }
+    }
+}
