3be2666bc3e47099712dddffb4cfe3f0e2d63f38
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-03-27 21:51:08 +0300

    Added Basic Algorithms - Exercises

---
+++ 19.BasicAlgorithms-Exercise/T01.RecursiveArraySum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace T01.RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(ArraySum(array, 0));
+        }
+
+        public static int ArraySum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            return array[index] + ArraySum(array, index + 1);
+        }
+    }
+}
---
+++ 19.BasicAlgorithms-Exercise/T02.RecursiveFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T02.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(num));
+        }
+
+        private static int Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+
+            return num * Factorial(num - 1);
+        }
+    }
+}
