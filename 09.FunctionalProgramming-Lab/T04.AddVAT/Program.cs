99703454674e1961c186a8b59f9032b610135b33
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-04-15 22:08:00 +0300

    solution edit

--- 09.FunctionalProgramming-Lab/T04.AddVAT/Program.cs
+++ 09.FunctionalProgramming-Lab/T04.AddVAT/Program.cs
@@ -9,8 +9,8 @@ namespace T04.AddVAT
         static void Main(string[] args)
         {
             List<decimal> prices = Console.ReadLine().Split(", ").Select(decimal.Parse).ToList();
-            Func<decimal, decimal> AddVat = x => x * 1.2m;
-            prices = prices.Select(AddVat).ToList();
+            Func<decimal, decimal> addVat = x => x * 1.2m;
+            prices = prices.Select(addVat).ToList();
             prices.ForEach(x => Console.WriteLine($"{x:f2}"));
         }
     }
